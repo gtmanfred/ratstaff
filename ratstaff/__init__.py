9110e7ff7768e9f5ae9e90fde3546c427f8cc14b
Daniel Wallace <danielwallace@gtmanfred.com>
2020-10-29 12:59:55 -0500

    remove poetry and update ratstaff
    Switch to using dicetray instead of xdice.  Also upgrade discord.py.

--- ratstaff/__init__.py
+++ ratstaff/__init__.py
@@ -1,28 +1,42 @@
 # -*- coding: utf-8 -*-
 import asyncio
+import logging
+import sys
+
 import click
 import discord
-import xdice
+import dicetray
+import sly.lex
+
+log = logging.getLogger('discord')
 
 
 class RatStaff(discord.Client):
     def __init__(self):
         self.loop = asyncio.get_event_loop()
-        super(RatStaff, self).__init__(loop=self.loop)
+        super().__init__()
 
-    async def roll_dice(self, message):
+    async def roll_dice(self, message, request):
         roller = message.author.mention
-        request = message.content.strip('%')
-        result = xdice.roll(f'{request}')
-        await self.send_message(message.channel,
-                                f'{roller}: {result.format()} => {result}')
+        tray = dicetray.Dicetray(request)
+        try:
+            result = tray.roll()
+        except sly.lex.LexError:
+            return
+        await message.channel.send(
+            f'{roller}: {tray.format(verbose=True)} => {result}',
+        )
 
     async def on_message(self, message):
         if message.author == self.user:
             return
 
-        if message.content.startswith('%'):
-            asyncio.ensure_future(self.roll_dice(message), loop=self.loop)
+        content = message.content.lstrip('%')
+        if content.startswith('roll '):
+            self.loop.create_task(self.roll_dice(message, content[5:]))
+            self.loop.create_task(message.delete())
+        elif content[0].isdigit():
+            self.loop.create_task(self.roll_dice(message, content))
 
     async def on_ready(self):
         print('Logged in as')
@@ -31,8 +45,21 @@ class RatStaff(discord.Client):
         print('------')
 
 
+def _setup_logging(level):
+    log.setLevel(getattr(logging, level.upper(), 'INFO'))
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    log.addHandler(handler)
+
+
 @click.command()
 @click.option('token', '--token', '-t', required=True, envvar='RATSTAFF_TOKEN')
-def main(token):
-    rat = RatStaff()
-    rat.run(token)
+@click.option(
+    'level', '--log-level', '-l',
+    type=click.Choice(['INFO', 'DEBUG', 'WARNING', 'CRITICAL']),
+    default='INFO',
+    help='level of logs to print out',
+)
+def main(token, level):
+    _setup_logging(level)
+    RatStaff().run(token)
